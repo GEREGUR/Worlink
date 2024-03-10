8ce70d02315105a8c91c8a0e5465fce08e268aa5
George <georgerubaylo@gmail.com>
2024-03-11 00:10:06 +0300

    Need to fix Click Outside for mobile navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,34 +1,38 @@
 "use client";
 
-import { Twirl as Hamburger, Twirl } from "hamburger-react";
+import { Twirl } from "hamburger-react";
+import Link from "next/link";
 import { useState } from "react";
+import { useClickAway } from "react-use";
+import { useRef } from "react";
 
 const links = [
   {
     id: 1,
-    link: "home",
+    link: "Главная",
   },
   {
     id: 2,
-    link: "about",
+    link: "О нас",
   },
   {
     id: 3,
-    link: "portfolio",
+    link: "Каталог",
   },
   {
     id: 4,
-    link: "experience",
+    link: "Модели",
   },
   {
     id: 5,
-    link: "contact",
+    link: "Связь",
   },
 ];
 
 export const Navbar = () => {
   const [nav, setNav] = useState(false);
-  console.log(nav);
+  const ref = useRef(null);
+  useClickAway(ref, () => setNav(false));
 
   return (
     <nav className="fixed flex h-[10vh] w-full flex-row justify-between p-4 text-white">
@@ -56,10 +60,27 @@ export const Navbar = () => {
             key={id}
             className="nav-links link-underline cursor-pointer px-4 font-medium capitalize text-neutral-200 duration-200 hover:scale-105 hover:text-white"
           >
-            <a href={link}>{link}</a>
+            <Link href={link}>{link}</Link>
           </li>
         ))}
       </ul>
+      {nav && (
+        <ul
+          className="absolute right-0 top-0 flex h-screen w-[60vh] flex-col items-center justify-center bg-gradient-to-b from-black to-red-400 text-slate-100 transition-all duration-300 ease-out hover:scale-105 hover:text-white md:hidden"
+          onClick={() => setNav((prev) => !prev)}
+        >
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="cursor-pointer px-4 py-6 text-4xl capitalize"
+            >
+              <Link onClick={() => setNav(!nav)} href={link}>
+                {link}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </nav>
   );
 };
